caa05f236313e730e79bb4c93f615cd7068412b9
lazyjul <45462701+lazyjul@users.noreply.github.com>
2019-03-23 14:48:42 +0100

    yikers

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Otto-Bot',
+      version='1.0',
+      description='Python Discord Bot',
+      author='jul',
+      author_email='julislazy@gmail.com',
+     )
