dfd03b0e7ee76ec71ba80cf19eef7d9ddeb38c7a
lazyjul <45462701+lazyjul@users.noreply.github.com>
2019-05-23 22:23:56 +0200

    Update ottobot.py

--- ottobot.py
+++ ottobot.py
@@ -110,7 +110,7 @@ class commandclass:
             # The file token.json stores the user's access and refresh tokens, and is
             # created automatically when the authorization flow completes for the first
             # time.
-            store = file.Storage(os.environ["calendartoken"])
+            store = os.environ["calendartoken"]
             creds = store.get()
             if not creds or creds.invalid:
                 flow = client.flow_from_clientsecrets(os.environ["credentials"], self.scopes)
