8ca7f37240cf282256ecdd06f63b0a881d39fdf0
lazyjul <45462701+lazyjul@users.noreply.github.com>
2019-05-25 12:07:36 +0200

    Update ids.py

--- ids.py
+++ ids.py
@@ -1,7 +1,7 @@
 server = {
-    "jokeChannel": "581130918308085796",
+    "jokeChannel": "529402776782897153",
     "modRole": "548483636362608640",
     "adminRole": "549694997683765267",
-    "calendarChannel": "581130918308085796",
-    "newsChannel": "581130918308085796"
+    "calendarChannel": "530772664092983326",
+    "newsChannel": "529402776782897153"
 }
